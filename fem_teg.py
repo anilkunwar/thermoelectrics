958147eaafac8ca60937b0c678828145814fbc5d
Anil Kunwar <anilkunwar318@yahoo.com>
2023-06-08 11:41:26 +0200

    Update fem_teg.py

--- fem_teg.py
+++ fem_teg.py
@@ -1,40 +1,35 @@
-import os
 import streamlit as st
 import subprocess
 
-def run_elmer_solver(sif_file_path, mesh_file_path):
+def run_elmer_solver(sif_file_path):
     # Command to run ElmerSolver
     command = f"ElmerSolver {sif_file_path}"
 
-    # Change directory to the mesh file location
-    mesh_directory = os.path.dirname(mesh_file_path)
-    command = f"cd {mesh_directory} && {command}"
+    # Run the command using subprocess and capture the output
+    process = subprocess.Popen(
+        command,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        bufsize=1,
+        universal_newlines=True
+    )
 
-    # Run the command using subprocess
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = process.communicate()
-
-    # Print the output and error messages
-    st.text("Output:")
-    st.text(output.decode())
-
-    st.text("Error:")
-    st.text(error.decode())
+    # Display the output in the web interface
+    with st.empty():
+        for line in process.stdout:
+            st.text(line.strip())
 
 def main():
     st.title("ElmerSolver Web Interface")
-    st.write("Please provide the path to the SIF file and the mesh file.")
+    st.write("Please provide the path to the SIF file.")
 
     # File input
-    sif_file_path = st.text_input("~/femstlit/teg.sif")
-    mesh_file_path = st.text_input("~/femstlit/Mesh_2-t1h0a0t0")
+    sif_file_path = st.text_input("Path to SIF file")
 
-    if sif_file_path and mesh_file_path:
+    if sif_file_path:
         # Run ElmerSolver
-        run_elmer_solver(sif_file_path, mesh_file_path)
+        run_elmer_solver(sif_file_path)
 
 if __name__ == "__main__":
     main()
-
-
-
