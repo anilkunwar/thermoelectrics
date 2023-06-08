759f07bf79ddab07e28a639c3bf28e4c4e4113f2
Anil Kunwar <anilkunwar318@yahoo.com>
2023-06-08 11:59:40 +0200

    Update fem_teg.py

--- fem_teg.py
+++ fem_teg.py
@@ -1,10 +1,11 @@
 import subprocess
 import streamlit as st
+import os
 
 def run_command(command):
-    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, error = process.communicate()
-    return output.decode()
+    return output.decode(), error.decode()
 
 def main():
     st.title("Remote Ubuntu Terminal Command")
@@ -12,8 +13,15 @@ def main():
     command = st.text_input("Enter the command to run:")
     
     if st.button("Run"):
-        output = run_command(command)
+        os.environ["PATH"] += ":/use/bin/ElmerSolver"  # Replace "/path/to/directory" with the actual directory containing the executable
+        output, error = run_command(command)
+        
+        st.subheader("Output:")
         st.code(output)
+        
+        if error:
+            st.subheader("Error:")
+            st.code(error)
 
 if __name__ == "__main__":
     main()
