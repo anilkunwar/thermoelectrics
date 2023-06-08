f2302acefb996a9c8bece1fa37ac10505b21b124
Anil Kunwar <anilkunwar318@yahoo.com>
2023-06-08 12:34:49 +0200

    Update fem_teg.py

--- fem_teg.py
+++ fem_teg.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import subprocess
+import base64
 
 def run_program(program_path):
     try:
         result = subprocess.run(program_path, capture_output=True, text=True, shell=True)
         output = result.stdout.strip()
-        with st.beta_expander("Show Output"):
+        with st.expander("Show Output"):
             st.code(output, language='text')
         st.markdown(get_download_link(output), unsafe_allow_html=True)
     except FileNotFoundError:
