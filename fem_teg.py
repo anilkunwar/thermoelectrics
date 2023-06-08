5ca2060ebb8b7d0725ced2b12d3bf861b83d4041
Anil Kunwar <anilkunwar318@yahoo.com>
2023-06-08 12:33:50 +0200

    Update fem_teg.py

--- fem_teg.py
+++ fem_teg.py
@@ -3,15 +3,19 @@ import subprocess
 
 def run_program(program_path):
     try:
-        process = subprocess.Popen(program_path, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
-        log_frame = st.empty()
-
-        for line in process.stdout:
-            log_frame.text(line.strip())
-
-        process.wait()
+        result = subprocess.run(program_path, capture_output=True, text=True, shell=True)
+        output = result.stdout.strip()
+        with st.beta_expander("Show Output"):
+            st.code(output, language='text')
+        st.markdown(get_download_link(output), unsafe_allow_html=True)
     except FileNotFoundError:
         st.error(f"Error: Program '{program_path}' not found.")
+
+def get_download_link(output):
+    output_encoded = output.encode()
+    b64 = base64.b64encode(output_encoded).decode()
+    href = f'<a href="data:file/txt;base64,{b64}" download="output.txt">Download Output</a>'
+    return href
 
 def main():
     st.title("Program Runner")
