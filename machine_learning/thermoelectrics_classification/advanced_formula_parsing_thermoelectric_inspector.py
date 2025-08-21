8a6e3f47a582fd9e202d1839b85efd51c22338cd
Anil Kunwar <anilkunwar318@yahoo.com>
2025-08-21 21:29:36 +0200

    Update advanced_formula_parsing_thermoelectric_inspector.py

--- machine_learning/thermoelectrics_classification/advanced_formula_parsing_thermoelectric_inspector.py
+++ machine_learning/thermoelectrics_classification/advanced_formula_parsing_thermoelectric_inspector.py
@@ -51,12 +51,9 @@ plt.rcParams.update({
 })
 
 # Directory and logging setup
-DB_DIR = "/home/kindness/workstation/declarmima_members/anil_kunwar/projects/word_graph1/conferenceFME/debuggin1/material_type"
-logging.basicConfig(
-    filename=os.path.join(DB_DIR, 'thermoelectric_ner.log'),
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
+# Set up logging
+DB_DIR = os.path.dirname(os.path.abspath(__file__))
+logging.basicConfig(filename=os.path.join(DB_DIR, 'thermoelectric_ner_analysis.log'), level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Streamlit configuration
 st.set_page_config(page_title="Thermoelectric Material Classification Tool", layout="wide")
