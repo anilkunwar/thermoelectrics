8a8d47b1f8cf9b68bc2511a481205ef94f4ab9cd
Anil Kunwar <anilkunwar318@yahoo.com>
2025-08-28 00:04:52 +0200

    Update multitier_hierarchical_data_colorized_visualization.py

--- machine_learning/vae_regression/integrated_models/multitier_hierarchical_data_colorized_visualization.py
+++ machine_learning/vae_regression/integrated_models/multitier_hierarchical_data_colorized_visualization.py
@@ -396,7 +396,8 @@ def create_top_n_sunburst(df, top_n, colormap_choice, discrete_mode, show_labels
             if 'material' in df.columns:
                 df = df[~df["material"].isin(excluded_labels)]
             if 'classification' in df.columns:
-                df = df[~df["classification'].isin(excluded_labels)]
+                df = df[~df["classification"].isin(excluded_labels)]
+
         
         # Standardize column names
         if 'material' in df.columns and 'classification' in df.columns:
